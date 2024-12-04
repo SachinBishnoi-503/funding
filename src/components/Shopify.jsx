0114d07eacbef2e63a044ab81d7d8c1adfb507c0
SachinBishnoi-503 <bishnoisachin626@gmail.com>
2024-12-04 12:51:54 +0530

    shopify section completed

---
+++ src/components/Shopify.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import amazon from '../assets/images/svg/amazon.svg'
+import ebay from '../assets/images/svg/Ebay.svg'
+import bigcommerce from '../assets/images/svg/bigcommerce.svg'
+import shopify from '../assets/images/svg/shoify.svg'  
+
+const Shopify = () => {
+  return (
+      <div className='bg-charcoal w-full min-h-[124px]'>
+          <div className='container'>
+              <div className='gap-[52px] flex items-center justify-center mx-auto pt-[46px] pb-[54px]'>
+                  <p className='font-poppins text-sm font-medium text-white'>MAIN SUPPORTED PLATFORMS</p>
+                  <div className='flex gap-[52px] items-center'>
+                      <img src={amazon} alt="" />
+                      <img src={shopify} alt="" />
+                      
+                  </div>
+                  <div className='flex gap-[52px] items-center'>
+                      <img src={bigcommerce} alt="" />
+                      <img src={ebay} alt="" />
+                      
+                  </div>
+              </div>
+              
+          </div>
+          
+    </div>
+  )
+}
+
+export default Shopify
---
+++ tailwind.config.js
@@ -0,0 +1,55 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      container: {
+        center: true,
+        padding: "20px",
+        screens: {
+          'sm': "640px",
+          'md': "768px",
+          'lg': "1024px",
+          'xl': "1240px",
+        }
+      },
+      backgroundImage: {
+        'hero': "url('./assets/images/webp/hero-image.webp')",
+      },
+      colors: {
+        'violet-blue': "#4C57FF",
+        'charcoal': "#0E1122",
+        'purple': "#AD00FF",
+        'skyblue': "#1877F2",
+        'night-skyblue': "#61A0FF",
+        'charcol': "#474747",
+        'midnight-blue': "#010026",
+        'night-blue': "#000A18",
+      },
+
+      fontSize: {
+        'custom-sm': "28px",
+        'custom-lg': "30px",
+        'custom-xxl': "62px",
+        'custom-xl': "36px",
+        'custom-2xl': "85px",
+        'custom-3xl': "90px",
+
+      },
+      lineHeight: {
+        'custom-lg': "30px",
+        'custom-xxl': "66px",
+        'custom-xl': "36px",
+        'custom-2xl': "85px",
+        'custom-3xl': "90px",
+      },
+      fontFamily: {
+        'abril': "Abril Display, sans- serif",
+        'poppins': "Poppins, sans-serif",
+      },
+  },
+},
+  plugins: [],
+}
