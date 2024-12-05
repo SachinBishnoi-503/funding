925bb10b72c5ec6ca42942bb824767b1e2b4d897
SachinBishnoi-503 <bishnoisachin626@gmail.com>
2024-12-05 14:04:27 +0530

    footer responsive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -14,16 +14,20 @@ const Footer = () => {
                     <img src={logo} alt="" />
                     <p className='text-off-grey font-medium font-poppins text-sm max-w-[163px]'>8fig, 1717 W 6th St, Austin, Texas</p>
                 </div>
-                <div className='flex items-center lg:hidden'>
+                <div className='flex items-center max-lg:hidden'>
                     <img src={year} alt="" />
                     <p className='font-poppins font-medium text-sm text-off-grey'>2024 8fig - All Rights Reserved</p>
                 </div>
-                <div className='flex items-center gap-[40px]'>
+                <div className='flex items-center max-lg:gap-[24px] lg:gap-[40px] lg:pt-[1px] max-lg:mb-[68px]'>
                     <a href="https://www.facebook.com/" target='_blank'><img src={facebook} alt="" /> </a>
                     <a href="https://www.instagram.com/instagram/?hl=en" target='_blank'><img src={instgram} alt="" /> </a>
                     <a href="https://in.linkedin.com/" target='_blank'><img src={linkdin} alt="" /> </a>
                     <a href="https://x.com/?lang=en&mx=2" target='_blank'><img src={twitter} alt="" /> </a>
                 </div>
+            </div>
+            <div className='mx-auto text-center pb-[28px] lg:hidden'>
+                <p className='font-poppins font-medium text-sm text-off-grey'>2024 8fig - All Rights Reserved</p>
+
             </div>
         </div>
     )
